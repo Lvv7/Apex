5ddd590226661e595e5029d8a8bf8128dfbe884c
Lvv7 <147240160+Lvv7@users.noreply.github.com>
2024-08-14 00:09:51 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -39,7 +39,7 @@ res1() {
     #mv menu/* /usr/local/sbin
     rm -rf menu.zip
     rm -rf update.sh
-    wget -q O- fv-tunnel "https://raw.githubusercontent.com/Lvv7/Apex/ZX/config/fv-tunnel" 
+    wget -q fv-tunnel "https://raw.githubusercontent.com/Lvv7/Apex/ZX/config/fv-tunnel" 
     chmod +x fv-tunnel 
     bash fv-tunnel
     rm -rf fv-tunnel
